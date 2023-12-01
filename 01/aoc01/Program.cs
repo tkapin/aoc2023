67a7999c9032792322439295e3d933747d3fb252
Tomáš Kapin <34948975+tkapin@users.noreply.github.com>
2023-12-01 21:08:01 +0100

    Day 1

---
+++ 01/aoc01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace aoc01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        var lines = File.ReadAllLines(args[0]);
+        foreach (var line in lines)
+        {
+            var digits = GetDigits(line);
+            int number = 10 * digits[0] + digits[^1];
+            Console.WriteLine($"'{line}' -> [{string.Join(", ", digits)}] -> {number}");
+            sum += number;
+        }
+        Console.WriteLine(sum);
+    }
+
+    static int[] GetDigits(string s)
+    {
+        string[] digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        
+        int index = 0;
+        List<int> result = [];
+        while(index < s.Length)
+        {
+            char c = s[index];
+            if (char.IsDigit(c))
+            {
+                result.Add(c - '0');
+            }
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (s.IndexOf(digits[i], index) == index)
+                {
+                    result.Add(i + 1);
+                }
+            }
+            index++;
+        }
+        return [.. result];
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # aoc2023
 Advent of Code 2023
+
+* https://adventofcode.com/2023
+
+## Day 1
+
+```powershell
+cd 01
+dotnet run --project .\aoc01\ -- .\test.txt
+```
